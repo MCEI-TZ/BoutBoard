d6fac6004472df1c90dff8717762e820f97c6e0b
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2026-07-19 20:08:51 -0600

    build(dependencies): Create backend skeleton for boutboard project

---
+++ backend/boutboard/src/main/java/com/mcei_tz/boutboard/BoutboardApplication.java
@@ -0,0 +1,13 @@
+package com.mcei_tz.boutboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutboardApplication.class, args);
+	}
+
+}
---
+++ backend/boutboard/src/test/java/com/mcei_tz/boutboard/BoutboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcei_tz.boutboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
